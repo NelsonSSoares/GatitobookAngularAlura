cacb2550c1c232863a88e12ad8bcf0dc5dbde2d5
Nelson <punkpunkada@gmail.com>
2023-01-11 14:53:47 -0300

    ReativeForm

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class NovoUsuarioComponent {
 
+  public novoUsuarioForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService){}
+
+  ngOnInit(): void{
+    this.novoUsuarioForm = this.formBuilder.group({
+      email:[''],
+      fullName:[''],
+      userName:[''],
+      password:['']
+    })
+  }
+  public cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+    
+  }
 }
---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public cadastraNovoUsuario(novoUsuario: NovoUsuario){
+    return this.httpClient.post('http://localhost:3000/user/signup',novoUsuario);
+  }
+}
+
