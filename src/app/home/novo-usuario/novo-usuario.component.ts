e32a103c69ab954e159a44b2b43bbe3170d62b39
Nelson <punkpunkada@gmail.com>
2023-01-12 14:13:13 -0300

    Validação Padrão do Angular

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -16,11 +16,11 @@ export class NovoUsuarioComponent {
 
   ngOnInit(): void{
     this.novoUsuarioForm = this.formBuilder.group({
-      email:[''],
-      fullName:[''],
+      email:['',[Validators.required, Validators.email]],  //segundo elemento do array, que é outro array é para validações em formularios reativos
+      fullName:['',[Validators.required, Validators.minLength(4)]],
       userName:[''],
       password:['']
-    })
+    });
   }
   public cadastrar(){
     const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
