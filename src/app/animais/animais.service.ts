9ef4cdc7b039037e174e1b40d9c771bc83a9cdd2
Nelson <punkpunkada@gmail.com>
2023-02-11 17:08:20 -0300

    activatedRoute (bug)

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
 import { TokenService } from '../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 
@@ -21,6 +21,13 @@ export class AnimaisService {
     const headers = new HttpHeaders().append('x-access-token', token);
 
     return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`,{headers});
+  }
+
+  public buscaPorId(id: number):Observable<Animal>{
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+
+    return this.http.get<Animal>(`${API}/photos/${id}`,{headers});
   }
 
 }
--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Animal } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-detalhe-animal',
   templateUrl: './detalhe-animal.component.html',
   styleUrls: ['./detalhe-animal.component.css']
 })
+
 export class DetalheAnimalComponent {
 
+  animalId!: number;
+  animal$!: Observable<Animal>;
+
+  constructor(private animaisService: AnimaisService, private activatedRoute: ActivatedRoute){}
+
+  ngOninit(): void{
+    this.animalId = this.activatedRoute.snapshot.params.['animalId'];
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
+  }
 }
