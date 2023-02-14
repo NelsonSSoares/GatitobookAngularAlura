02989cc540996f756f5052d0ab976195e70962dc
Nelson <punkpunkada@gmail.com>
2023-02-14 13:06:48 -0300

    organização de modulos

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
@@ -11,18 +12,15 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent {
 
-  animais$!: Observable<Animais>;
+  animais!: Animais;
   /*
     vamos declarar o atributo animais!:. Por que exclamação dois pontos? Porque eu vou instanciá-lo no ngOnInit. Como eu só estou declarando, tenho que colocar essa exclamação dois pontos, e ele vai ser do tipo Animais;.
   */
-  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService){}
+  constructor(private activatedRoute: ActivatedRoute){}
 
   ngOnInit(): void{
-    this.animais$ = this.usuarioService.retornaUsuario().pipe(
-      switchMap((usuario)=>{
-        const userName = usuario.name ?? '';
-        return this.animaisService.listaDoUsuario(userName);
-      })
-    )
+    this.activatedRoute.params.subscribe(param => {
+      this.animais = this.activatedRoute.snapshot.data['animais'];
+    })
   }
 }
