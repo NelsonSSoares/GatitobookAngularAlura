353004bb1e81db1bbec7f7b72da0d9f3a6fcab44
Nelson <punkpunkada@gmail.com>
2023-02-14 15:50:58 -0300

    form novo animal

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novo-animal',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class NovoAnimalComponent {
 
+  formularioAnimal!: FormGroup;
+  file!: File;
+  preview!: string; 
+  percentualConcluido = 0;
+
+  constructor(){}
+
+  public upload(){
+
+  }
+
+  public gravaArquivo(arquivo: any){
+
+  }
 }
